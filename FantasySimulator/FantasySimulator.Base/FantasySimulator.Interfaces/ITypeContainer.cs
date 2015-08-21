727acf6cf597d34a89c1725ab85fdc7cd472b9c1
Peter Åslund <peter.aslund@hotmail.com>
2015-08-21 21:28:51 +0200

    Interfaces

---
+++ FantasySimulator/FantasySimulator.Base/FantasySimulator.Interfaces/ITypeContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FantasySimulator.Interfaces
+{
+    public interface ITypeContainer : ITypeResolver
+    {
+        void SetInstance(object instance);
+        void SetInstance(Type type, object instance);
+        void SetInstance<T>(T instance);
+    }
+}
---
+++ FantasySimulator/FantasySimulator.Base/FantasySimulator.Interfaces/ITypeResolver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FantasySimulator.Interfaces
+{
+    public interface ITypeResolver
+    {
+        T GetInstance<T>();
+        object GetInstance(Type type);
+    }
+}
