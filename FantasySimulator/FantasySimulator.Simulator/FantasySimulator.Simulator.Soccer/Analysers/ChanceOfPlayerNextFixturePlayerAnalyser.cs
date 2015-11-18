8688ce48fd797d6e472870dbbea5671e1db1eb0c
Peter Åslund <peter.aslund@hotmail.com>
2015-11-18 17:53:32 +0100

    daily

--- FantasySimulator/FantasySimulator.Simulator/FantasySimulator.Simulator.Soccer/Analysers/ChanceOfPlayerNextFixturePlayerAnalyser.cs
+++ FantasySimulator/FantasySimulator.Simulator/FantasySimulator.Simulator.Soccer/Analysers/ChanceOfPlayerNextFixturePlayerAnalyser.cs
@@ -4,6 +4,17 @@ namespace FantasySimulator.Simulator.Soccer.Analysers
 {
     public class ChanceOfPlayerNextFixturePlayerAnalyser : PlayerAnalyserBase
     {
+        public ChanceOfPlayerNextFixturePlayerAnalyser()
+        {
+            
+        }
+
+        public PointsRange PointsRange
+        {
+            get { return Properties["Points"].SafeConvert<int>(); }
+            set { Properties["Points"] = value; }
+        }
+
         public override IEnumerable<PlayerRecommendation> Analyse(Player player, Fixture fixture, SimulationContext context)
         {
             var res = new PlayerRecommendation();
@@ -22,6 +33,32 @@ namespace FantasySimulator.Simulator.Soccer.Analysers
                     res.Points = -1;
             }
             yield return res;
+        }
+
+        public class RangeMapping
+        {
+            public RangePredicate[] Predicates { get; set; }
+        }
+
+        public class PointsRange : RangeMapping
+        {
+            public int Points { get; set; }
+        }
+
+        public class RangePredicate
+        {
+            public ComparisonOperator Operator { get; set; }
+            public double Value { get; set; }
+        }
+
+        public enum ComparisonOperator
+        {
+            None,
+            LessThan,
+            LessOrEqualThan,
+            Equals,
+            GreaterOrEqualThan,
+            GreaterThan,
         }
     }
 }
