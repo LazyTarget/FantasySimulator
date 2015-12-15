632455a46f9e8c5c4fff6b679ddf758bf3fbc88b
LazyTarget <peter.aslund@hotmail.com>
2015-12-15 18:19:22 +0100

    Updated TransfersPage data

--- FantasySimulator/FantasySimulator.StartUp/FantasySimulator.DebugConsole/Data/FantasyPremierLeagueDataFactory.cs
+++ FantasySimulator/FantasySimulator.StartUp/FantasySimulator.DebugConsole/Data/FantasyPremierLeagueDataFactory.cs
@@ -14,7 +14,7 @@ namespace FantasySimulator.DebugConsole.Data
 {
     public class FantasyPremierLeagueDataFactory : ISoccerSimulationDataFactory
     {
-        private static string TransfersPageJsonFilename = "TransfersPage_201512021752.json";
+        private static string TransfersPageJsonFilename = "TransfersPage_201512151815.json";
         private const string GetTransferDataJsonUrl     = "http://fantasy.premierleague.com/transfers/";
         private const string GetPlayerDataJsonUrl       = "http://fantasy.premierleague.com/web/api/elements/";
         private const string GetFixturesJsonUrl         = "http://api.football-data.org/alpha/soccerseasons/398/fixtures";
