0eb05fe5e3d1d62afeb814473cc2987b9e40adaa
Peter Åslund <peter.aslund@hotmail.com>
2016-01-08 09:32:30 +0100

    Implemented FantasyPremierLeagueApiDataFactory which queries the api directly (through NuGet package) Implemented IXmlConfigurable for FantasyPremierLeagueDataFactory Refactored TransfersPageJsonFilename setting

---
+++ FantasySimulator/FantasySimulator.Base/FantasySimulator.Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace FantasySimulator.Core
+{
+    public static class CollectionExtensions
+    {
+        public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            var value = dictionary[key];
+            return value;
+        }
+
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            var value = default(TValue);
+            if (dictionary.ContainsKey(key))
+                value = dictionary.Get(key);
+            return value;
+        }
+    }
+}
